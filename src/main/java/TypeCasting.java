3754ea75ccd71a975c451e03b85fd2e4b9aedfdc
Mynul <mamun.harf@gmail.com>
2026-04-18 00:48:51 +0600

    init fileName

--- src/main/java/TypeCasting.java
+++ src/main/java/TypeCasting.java
@@ -1,5 +1,13 @@
 public class TypeCasting {
     static void main() {
 
+        int num1, num2, result;
+
+        num1 = 10;
+        num2 = 30;
+
+        result = num1 + num2;
+        System.out.println("Result:"+result);
+
     }
 }
