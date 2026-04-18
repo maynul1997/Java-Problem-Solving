43aaf413c3be403cc3ee5bf88a7d254b29fee85f
Mynul <mamun.harf@gmail.com>
2026-04-18 15:02:14 +0600

    Print User guide

--- src/main/java/Assignment4.java
+++ src/main/java/Assignment4.java
@@ -5,6 +5,8 @@ public class Assignment4 {
     static void main() {
         int phonePrice = 1800; //Euros
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter the Number of Installment: ");
+        int installment = input.nextInt();
     }
 }
