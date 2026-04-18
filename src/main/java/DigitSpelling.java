a7da7ef9bedb60e7072433860ef378cf87823e27
Mynul <mamun.harf@gmail.com>
2026-04-19 01:10:00 +0600

    Print part

--- src/main/java/DigitSpelling.java
+++ src/main/java/DigitSpelling.java
@@ -9,7 +9,11 @@ public class DigitSpelling {
             int digit = num % 10 ;
             reverse = reverse * 10 + digit;
             num = num / 10;
+        }
 
+        // Print part
+        while(reverse>0) {
+            int digit = reverse % 10;
         }
 
     }
