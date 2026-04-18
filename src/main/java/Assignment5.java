a14ad6fa8089a95b3bab8bad00421994488ec9ec
Mynul <mamun.harf@gmail.com>
2026-04-18 19:09:37 +0600

    Input age

--- src/main/java/Assignment5.java
+++ src/main/java/Assignment5.java
@@ -5,6 +5,9 @@ public class Assignment5 {
     static void main() {
 
         Scanner input = new Scanner(System.in);
+        int age;
 
+        System.out.println("Enter your age: ");
+        age = input.nextInt();
     }
 }
