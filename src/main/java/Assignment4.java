05a753fee16ed62a0c8f1c9788d4b1aed25a27e1
Mynul <mamun.harf@gmail.com>
2026-04-18 14:58:47 +0600

    User input

--- src/main/java/Assignment4.java
+++ src/main/java/Assignment4.java
@@ -1,5 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Assignment4 {
     static void main() {
-
+        int phonePrice = 1800; //Euros
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Number of Installment: ");
     }
 }
