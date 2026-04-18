77b98bf9245b170606a806655efd2d3a71adee34
Mynul <mamun.harf@gmail.com>
2026-04-18 15:15:55 +0600

    Phone Price print

--- src/main/java/Assignment4.java
+++ src/main/java/Assignment4.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 
 public class Assignment4 {
     static void main() {
-        int phonePrice = 18000; //Taka
-        int NumOfInstallment, InstallmentPerMonth;
+
+        int phonePrice, NumOfInstallment, InstallmentPerMonth;
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the Price of Phone: ");
 
         System.out.println("Enter the Number of Installment: ");
         NumOfInstallment = input.nextInt();
 
         InstallmentPerMonth = phonePrice/NumOfInstallment;
-        System.out.println("Per Month Installment: "+InstallmentPerMonth+"Taka");
+        System.out.println("Per Month Installment: "+InstallmentPerMonth+ "Taka");
     }
 }
