fc20b0e5bdd2e968452a08830c900ef80ba8dd98
Mynul <mamun.harf@gmail.com>
2026-04-18 18:09:38 +0600

    Fer theory

--- src/main/java/Temperature_Converter.java
+++ src/main/java/Temperature_Converter.java
@@ -4,10 +4,12 @@ public class Temperature_Converter {
     static void main() {
 
         Scanner input = new Scanner(System.in);
-        double cels;
+        double cels, Fer;
 
         System.out.println("Enter Cels Temperature: ");
         cels = input.nextDouble();
+
+        Fer = 9/5(cels-32);
 
     }
 }
