b9c4dc146bed0aa25a7babc074112084b24edbc7
Mynul <mamun.harf@gmail.com>
2026-04-18 17:03:03 +0600

    area theory

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Circle {
     static void main() {
         Scanner input = new Scanner(System.in);
-        double radius;
+        double radius, area;
 
         System.out.println("Enter radius: ");
         radius = input.nextDouble();
+
+        area = 3.1416 * radius * radius;
+
 
 
 
