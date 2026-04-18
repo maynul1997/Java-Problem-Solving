5047a54da4b25447ed9c1182eec00fe1dbc5b095
Mynul <mamun.harf@gmail.com>
2026-04-18 16:31:18 +0600

    x Minus value print

--- src/main/java/Assignment_Operator.java
+++ src/main/java/Assignment_Operator.java
@@ -4,7 +4,13 @@ public class Assignment_Operator {
         int x = 10;
         int y = 20;
 
-        x += y;
-       System.out.println("x ="+x);
+        x += y; // x = x + y; x = 10 + 20;
+       System.out.println("x = "+x);
+
+       x -= y;
+       System.out.println("x = "+x);
+
+
+
     }
 }
