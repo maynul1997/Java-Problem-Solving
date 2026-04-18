887b5464b842a21821220ee2f06ec32dfd3be84c
Mynul <mamun.harf@gmail.com>
2026-04-18 16:52:58 +0600

    Base of triangle

--- src/main/java/Area_of_triangle.java
+++ src/main/java/Area_of_triangle.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Area_of_triangle {
     static void main() {
+        double base, height, area;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Base: ");
+        base = input.nextDouble();
+
+
+
+
+
 
     }
 }
