230ca33add68198072d86d2f9a7631777fed6c56
Mynul <mamun.harf@gmail.com>
2026-04-20 01:28:15 +0600

    input

--- src/main/java/Assignment_7.java
+++ src/main/java/Assignment_7.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 
 public class Assignment_7 {
-    static void main() {
+    public static void main(String[] args) {
 
-        Scanner input = new Scanner (System.in);
-        char ch ;
-        char num ;
+        Scanner input = new Scanner(System.in);
+        char ch;
+        char num;
         System.out.println("Enter Y or N : ");
-        ch = input.next().charAt();
+        ch = input.next().charAt(0);
 
         System.out.println("Enter Y or N : ");
-        num = input.next().charAt();
+        num = input.next().charAt(0);
 
-        if(ch =='Y' || ch == 'y' ){
+        if (ch == 'Y' || ch == 'y') {
             System.out.println("I love Java");
-        }
-
-        else if (num == 'N' || num == 'n'){
+        } else if (num == 'N' || num == 'n') {
             System.out.println("I donot love Java");
         }
 
