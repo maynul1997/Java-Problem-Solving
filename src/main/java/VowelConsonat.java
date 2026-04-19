d2390a29a772f8f910e2ee5215af654d3e568322
Mynul <mamun.harf@gmail.com>
2026-04-19 23:34:28 +0600

    Check mistake

--- src/main/java/VowelConsonat.java
+++ src/main/java/VowelConsonat.java
@@ -8,23 +8,23 @@ public class VowelConsonat {
         System.out.println("Enter a Letter: ");
         ch = input.next().charAt(0);
 
-        if(ch=="a"){
+        if(ch=='a'){
             System.out.println("Vowel");
         }
 
-        else if(ch=="e"){
+        else if(ch=='e'){
             System.out.println("Vowel");
         }
 
-        else if(ch=="i"){
+        else if(ch=='i'){
             System.out.println("Vowel");
         }
 
-        else if(ch=="o"){
+        else if(ch=='o'){
             System.out.println("Vowel");
         }
 
-        else if(ch=="u"){
+        else if(ch=='u'){
             System.out.println("Vowel");
         }
 
