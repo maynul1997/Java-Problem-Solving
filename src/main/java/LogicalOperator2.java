217250000b4e8294c02b9e956fcea3ad0e1b8cf2
Mynul <mamun.harf@gmail.com>
2026-04-20 00:58:20 +0600

    scanner

--- src/main/java/LogicalOperator2.java
+++ src/main/java/LogicalOperator2.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class LogicalOperator2 {
 
     static void main() {
 
+        Scanner input = new Scanner(System.in);
+        char ch;
+
+
+
+        if ()
     }
 }
