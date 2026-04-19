a3cbf5aa619273d00c204df6224891db764007af
Mynul <mamun.harf@gmail.com>
2026-04-19 23:28:00 +0600

    Scanner charAt

--- src/main/java/VowelConsonat.java
+++ src/main/java/VowelConsonat.java
@@ -4,6 +4,9 @@ public class VowelConsonat {
     static void main() {
         Scanner input = new Scanner(System.in);
         char ch;
+
         System.out.println("Enter a Letter: ");
+        ch = input.next().charAt(0);
+
     }
 }
