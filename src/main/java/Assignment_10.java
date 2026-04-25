033b755fb79a4a9b68c3e61e40479f8971cd3eda
Mynul <mamun.harf@gmail.com>
2026-04-25 23:11:19 +0600

    i print

--- src/main/java/Assignment_10.java
+++ src/main/java/Assignment_10.java
@@ -1,6 +1,10 @@
 public class Assignment_10 {
     static void main() {
         int i;
-        for (i = 1; i < 10; i++)
+        for (i = 1; i < 10; i++){
+
+        }
+        System.out.println("Addition of all values: "+);
+
     }
 }
