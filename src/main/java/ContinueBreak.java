3cc48dd2e478e700e8fadda7865842b273b7b427
Mynul <mamun.harf@gmail.com>
2026-04-26 01:23:40 +0600

    continue using

--- src/main/java/ContinueBreak.java
+++ src/main/java/ContinueBreak.java
@@ -5,8 +5,13 @@ public class ContinueBreak {
         for(i = 1; i<=100; i++){
 
             if(i==10){
+                continue;
+            }
+
+            if(i>15){
                 break;
             }
+
         }
     }
 }
