054768f6a51b7574a5b51b5be6952552195c9511
Mynul <mamun.harf@gmail.com>
2026-04-25 14:57:05 +0600

    absolute value

--- src/main/java/Demo_math.java
+++ src/main/java/Demo_math.java
@@ -6,8 +6,12 @@ public class Demo_math {
         int max = Math.max(a,b);
         System.out.println("Maximum value print = "+max);
 
-        int min = Math.min(a,b);
-        System.out.println("Minimun value Print= "+min);
+        int minimum = Math.min(a,b);
+        System.out.println("Minimun value Print= "+minimum);
+
+        int absolute = Math.abs(a,b);
+
+
 
 
     }
