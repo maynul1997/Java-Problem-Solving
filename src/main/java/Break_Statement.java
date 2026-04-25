8463c7b4af9af2a14255941f3c2a03911bf6071b
Mynul <mamun.harf@gmail.com>
2026-04-26 01:16:35 +0600

    logic change

--- src/main/java/Break_Statement.java
+++ src/main/java/Break_Statement.java
@@ -2,12 +2,14 @@ public class Break_Statement {
     static void main() {
         int i;
 
-        for(i = 0; i >= 100; i++){
+        for (i = 0; i <= 100; i++) {
 
-            if (i == 50)
+            if (i == 10) {
                 break;
-            System.out.println(i);
-        }
 
+            }
+            System.out.println(i);
+
+        }
     }
 }
