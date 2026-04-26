5176e9d081ee5d81d4ccc0deec59ccc0466e0f49
Mynul <mamun.harf@gmail.com>
2026-04-27 00:39:50 +0600

    input num1

--- src/main/java/SumOFLoop.java
+++ src/main/java/SumOFLoop.java
@@ -4,6 +4,9 @@ public class SumOFLoop {
     static void main() {
         Scanner input = new Scanner(System.in);
 
+        int num1, num2, i;
+        System.out.println("Enter num1 ");
+        num1 = input.nextInt();
 
     }
 }
