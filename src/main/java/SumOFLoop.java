962bcf3f5b53d47a1c69400c46841d55c82fd373
Mynul <mamun.harf@gmail.com>
2026-04-27 00:41:36 +0600

    loop using

--- src/main/java/SumOFLoop.java
+++ src/main/java/SumOFLoop.java
@@ -5,11 +5,15 @@ public class SumOFLoop {
         Scanner input = new Scanner(System.in);
 
         int num1, num2, i;
-        System.out.println("Enter num1 ");
+        System.out.println("Enter num1 : ");
         num1 = input.nextInt();
 
-        System.out.println("Enter num1 ");
+        System.out.println("Enter num2 :  ");
         num2 = input.nextInt();
+
+        for( i = num1; i <num2; i++){
+
+        }
 
     }
 }
