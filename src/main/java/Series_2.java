51c4279d21abca5ca835c62ef4cc7dbc68c72723
Mynul <mamun.harf@gmail.com>
2026-04-29 00:14:57 +0600

    mul variable added

--- src/main/java/Series_2.java
+++ src/main/java/Series_2.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Series_2 {
     static void main() {
         Scanner input = new Scanner(System.in);
-        int n;
+        int n, mul=1 ;
 
         System.out.println("Enter the last number : ");
         n = input.nextInt();
 
-        for (int i = 0 ; i<= n; i++)
+        for (int i = 0 ; i<= n; i++){
+
+        }
 
 
     }
