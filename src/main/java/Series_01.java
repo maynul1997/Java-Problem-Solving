97d53ec37fc92ae4098ab6b3e69e2e7d25b72597
Mynul <mamun.harf@gmail.com>
2026-04-28 23:42:33 +0600

    logic change again logic

--- src/main/java/Series_01.java
+++ src/main/java/Series_01.java
@@ -9,7 +9,7 @@ public class Series_01 {
         System.out.println("Enter the series of num : ");
         num = input.nextInt();
 
-        for(int i=1; i<=10; i=i+1) {
+        for(int i=1; i<=num; i=i+1) {
             sum = sum + i;
 
         }
