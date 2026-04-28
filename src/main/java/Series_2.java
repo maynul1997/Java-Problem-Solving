8102e3545afdf939608dcf6cc4f9b0629e676e54
Mynul <mamun.harf@gmail.com>
2026-04-29 00:16:28 +0600

    change the logic

--- src/main/java/Series_2.java
+++ src/main/java/Series_2.java
@@ -8,7 +8,7 @@ public class Series_2 {
         System.out.println("Enter the last number : ");
         n = input.nextInt();
 
-        for (int i = 0 ; i<= n; i++){
+        for (int i = 1 ; i<= n; i++){
             mul = mul * i;
         }
         System.out.println(mul);
