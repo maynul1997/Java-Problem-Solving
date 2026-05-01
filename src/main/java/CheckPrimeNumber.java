fd3d206f3a4af134b95109acd59b33c09294ee2e
Mynul <mamun.harf@gmail.com>
2026-05-01 15:20:12 +0600

    message print for user

--- src/main/java/CheckPrimeNumber.java
+++ src/main/java/CheckPrimeNumber.java
@@ -4,6 +4,8 @@ public class CheckPrimeNumber {
     static void main() {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter any positive number : ");
+
 
     }
 }
