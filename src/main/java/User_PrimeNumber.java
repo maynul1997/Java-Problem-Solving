a5cfe8c9c99a8d3165938c63c9868e6b5e7c6c35
Mynul <mamun.harf@gmail.com>
2026-05-02 01:06:03 +0600

    fivonaacci number

--- src/main/java/User_PrimeNumber.java
+++ src/main/java/User_PrimeNumber.java
@@ -13,6 +13,7 @@ public class User_PrimeNumber {
         n = input.nextInt();
 
         for (int i= m; i<n; i++){
+
             for( j=2; j<i; j++){
                 if( i%j == 0) {
                     count ++;
---
+++ src/main/java/fivonacci.java
@@ -0,0 +1,5 @@
+public class fivonacci {
+    static void main() {
+
+    }
+}
