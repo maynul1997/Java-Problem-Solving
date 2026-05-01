1a05274316c6e7cc9e2785f05f8b431c9e6f4cf3
Mynul <mamun.harf@gmail.com>
2026-05-02 01:23:04 +0600

    using space

--- src/main/java/fivonacci.java
+++ src/main/java/fivonacci.java
@@ -12,10 +12,10 @@ public class fivonacci {
         int fibo;
         System.out.print(first+" "+second);
 
-        for( int i= 3; i<n; i++ ){
+        for( int i= 3; i<=n; i++ ){
 
             fibo = first + second;
-            System.out.print(fibo);
+            System.out.print(" "+fibo);
 
             first = second;
             second = fibo;
