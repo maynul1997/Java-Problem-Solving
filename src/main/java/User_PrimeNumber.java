148dba018de2396691ec4a811fc429fa918f6c7d
Mynul <mamun.harf@gmail.com>
2026-05-01 19:14:26 +0600

    assign to variable

--- src/main/java/User_PrimeNumber.java
+++ src/main/java/User_PrimeNumber.java
@@ -4,6 +4,7 @@ public class User_PrimeNumber {
     static void main() {
         Scanner input = new Scanner(System.in);
          int m, n;
-
+        System.out.println("Enter the initial number : ");
+        m = input.nextInt();
     }
 }
