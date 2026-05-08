295d0066dfc42e91dc648cff3b627ea29a023262
Mynul <mamun.harf@gmail.com>
2026-05-09 00:58:26 +0600

    print message for user

--- src/main/java/ReverseOf_Digit.java
+++ src/main/java/ReverseOf_Digit.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class ReverseOf_Digit {
     static void main() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+
 
 
     }
