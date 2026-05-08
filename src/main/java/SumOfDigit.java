6561eebf5f7d5c15d0f3511996c1895f27d44dc9
Mynul <mamun.harf@gmail.com>
2026-05-08 22:32:40 +0600

    variable declare

--- src/main/java/SumOfDigit.java
+++ src/main/java/SumOfDigit.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class SumOfDigit {
     static void main() {
         Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int temp;
         System.out.println("Enter the Digit : ");
+
 
 
     }
