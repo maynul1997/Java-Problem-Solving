69520dea15b5189f073b88eba82ae760b5d9e3ea
Mynul <mamun.harf@gmail.com>
2026-05-08 22:28:46 +0600

    user input

--- src/main/java/SumOfDigit.java
+++ src/main/java/SumOfDigit.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SumOfDigit {
     static void main() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Digit : ");
 
 
     }
