e0b4c7ec59933cc9fa6ead9e17111a95d571946c
Mynul <mamun.harf@gmail.com>
2026-05-09 01:05:28 +0600

    variable declare r

--- src/main/java/ReverseOf_Digit.java
+++ src/main/java/ReverseOf_Digit.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class ReverseOf_Digit {
     static void main() {
         Scanner input = new Scanner(System.in);
-        int temp, num, sum =0;
+        int temp, r, num, sum =0;
+
         System.out.println("Enter the number : ");
+
+
 
 
 
