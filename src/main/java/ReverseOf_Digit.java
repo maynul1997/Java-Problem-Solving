3911744fab010973bc4adc0a5cb76db6a1641f4e
Mynul <mamun.harf@gmail.com>
2026-05-09 01:14:17 +0600

    print result

--- src/main/java/ReverseOf_Digit.java
+++ src/main/java/ReverseOf_Digit.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ReverseOf_Digit {
     static void main() {
         Scanner input = new Scanner(System.in);
-        int temp, r, num, sum =0;
+        int temp, r=0 , num, sum =0;
 
         System.out.println("Enter the number : ");
         num = input.nextByte();
@@ -16,6 +16,8 @@ public class ReverseOf_Digit {
 
 
         }
+
+        System.out.println("The reverse number is : "+r);
 
 
     }
