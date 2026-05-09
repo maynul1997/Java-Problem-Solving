78d9815ea602c56e64a23d45b411d88b26714170
Mynul <mamun.harf@gmail.com>
2026-05-09 23:52:31 +0600

    variable declare

--- src/main/java/Armstrong_NUmber.java
+++ src/main/java/Armstrong_NUmber.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Armstrong_NUmber {
     static void main() {
         Scanner input = new Scanner(System.in);
+        int num, r, temp, sum= 0;
 
         System.out.println("Enter the number: ");
+
 
 
     }
