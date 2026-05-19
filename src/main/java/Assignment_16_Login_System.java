d22a02e4d75374672d532066faaf117fdb56a056
Mynul <mamun.harf@gmail.com>
2026-05-19 23:48:29 +0600

    using is condition

--- src/main/java/Assignment_16_Login_System.java
+++ src/main/java/Assignment_16_Login_System.java
@@ -13,7 +13,11 @@ public class Assignment_16_Login_System {
             username = input.nextLine();
 
             System.out.println("Enter password : ");
+            password = input.nextLine();
 
+            if(username.equals("Anis") && password.equals(123456)) {
+                System.out.println("Welcome to the System");
+            }
         }
 
 
