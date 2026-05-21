6d4f76bdebd59c91f96edd76a94b5856ff67fd1c
Mynul <mamun.harf@gmail.com>
2026-05-21 23:51:52 +0600

    print col

--- src/main/java/Pattern_01.java
+++ src/main/java/Pattern_01.java
@@ -9,7 +9,7 @@ public class Pattern_01 {
 
         for( int row = 1; row <= n; row++ ) {
             for( int col = 1; col <= row; col++ ) {
-
+                System.out.println(" "+col);
 
             }
 
