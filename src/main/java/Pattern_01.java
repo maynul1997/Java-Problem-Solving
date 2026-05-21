698ce5dd24c45af8ee3b40af87ac2d192b214188
Mynul <mamun.harf@gmail.com>
2026-05-21 23:54:36 +0600

    code decorate three

--- src/main/java/Pattern_01.java
+++ src/main/java/Pattern_01.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Pattern_01 {
     static void main() {
+
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter line number : ");
