c33219ca2447d3f52da115f4a3ed20b72fd4ef85
Mynul <mamun.harf@gmail.com>
2026-05-22 23:58:53 +0600

    change the mistake

--- src/main/java/Pattern_02.java
+++ src/main/java/Pattern_02.java
@@ -10,7 +10,7 @@ public class Pattern_02 {
 
         int n = input.nextInt();
 
-        for ( int row = n; row >= 1; row++) {
+        for ( int row = n; row >= 1; row--) {
             for (int col = 1; col <= row; col++) {
                 System.out.print(" "+col);
 
