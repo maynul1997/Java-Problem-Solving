e0f1d1c761045b6981323192597148d3fc16261d
Mynul <mamun.harf@gmail.com>
2026-05-22 23:56:52 +0600

    second ln pront

--- src/main/java/Pattern_02.java
+++ src/main/java/Pattern_02.java
@@ -12,9 +12,11 @@ public class Pattern_02 {
 
         for ( int row = n; row >= 1; row++) {
             for (int col = 1; col <= row; col++) {
-                System.out.println(" "+col);
+                System.out.print(" "+col);
 
             }
+
+            System.out.println();
 
         }
 
