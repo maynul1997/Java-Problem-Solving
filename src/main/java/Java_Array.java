fe667d86a3a06cb8c424574caee6f5f3a4c7a5c1
Mynul <mamun.harf@gmail.com>
2026-05-26 15:39:51 +0600

    initiate valuse fo array

--- src/main/java/Java_Array.java
+++ src/main/java/Java_Array.java
@@ -3,6 +3,14 @@ public class Java_Array {
     static void main() {
         int [] number = new int[5];
 
+        number [0] = 10;
+        number [1] = 10;
+        number [2] = 10;
+        number [3] = 10;
+        number [4] = 10;
+        number [5] = 10;
+        number [6] = 10;
+
     }
 
 
