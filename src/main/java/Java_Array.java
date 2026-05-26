bcaf49ae7dc7327338ac941d0ba344ba8d2b3dbf
Mynul <mamun.harf@gmail.com>
2026-05-26 16:01:46 +0600

    print result

--- src/main/java/Java_Array.java
+++ src/main/java/Java_Array.java
@@ -3,13 +3,15 @@ public class Java_Array {
     static void main() {
         int [] number = new int[5];
 
-        number [0] = 10;
+        number [0] = 15;
         number [1] = 10;
         number [2] = 10;
         number [3] = 10;
         number [4] = 10;
         number [5] = 10;
         number [6] = 10;
+
+        System.out.println(number[0]);
 
     }
 
