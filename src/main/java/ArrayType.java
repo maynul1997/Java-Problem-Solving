1896996e0a6ff7d2a25628f9a1aea0fd7c75bace
Mynul <mamun.harf@gmail.com>
2026-05-28 00:31:43 +0600

    print number

--- src/main/java/ArrayType.java
+++ src/main/java/ArrayType.java
@@ -3,6 +3,13 @@ public class ArrayType {
 
         int [][] number = new int [2][4];
 
+        number [0][0] = 10;
+        number [0][1] = 10;
+        number [0][2] = 10;
+        number [1][0] = 10;
+        number [1][1] = 10;
+        number [1][2] = 10;
+
     }
 
 }
