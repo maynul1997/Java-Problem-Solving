56c1de164b69af7b5b317e2af144982a5a9c64d8
Mynul <mamun.harf@gmail.com>
2026-05-27 12:52:56 +0600

    user input loop update

--- src/main/java/Array_Sum.java
+++ src/main/java/Array_Sum.java
@@ -8,11 +8,9 @@ public class Array_Sum {
         double sum = 0;
         System.out.println("Please Enter the numbers : ");
 
-        number [0] = input.nextDouble();
-        number [1] = input.nextDouble();
-        number [2] = input.nextDouble();
-        number [3] = input.nextDouble();
-        number [4] = input.nextDouble();
+        for( int i = 0; i < 5; i++) {
+            number [0] = input.nextDouble();
+        }
 
         sum = number [0] + number [1] + number [2] + number [3] + number [4];
         System.out.println("The Sum is : "+sum);
