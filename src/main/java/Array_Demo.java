c2d878322690a229f70faa5e68a398afc2598c5b
Mynul <mamun.harf@gmail.com>
2026-05-29 23:12:55 +0600

    adding print for

--- src/main/java/Array_Demo.java
+++ src/main/java/Array_Demo.java
@@ -4,15 +4,18 @@ public class Array_Demo {
         int [][] number = new int [4] [5];
         int k = 0;
 
+        //Assigning the value to the 2 Dimentional Array
         for ( int row = 0; row < 4; row ++ ) {
             for ( int col = 0; col < 5; col++ ) {
                 number [row][col] = k;
                 k++;
 
-
             }
 
         }
+        //Printing the elements
+        System.out.println();
+
 
     }
 
