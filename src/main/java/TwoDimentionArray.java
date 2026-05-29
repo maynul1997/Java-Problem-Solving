f0016978f7645bde0769bdfb543639d124c18987
Mynul <mamun.harf@gmail.com>
2026-05-29 09:37:01 +0600

    print

--- src/main/java/TwoDimentionArray.java
+++ src/main/java/TwoDimentionArray.java
@@ -4,22 +4,27 @@ public class TwoDimentionArray {
     static void main() {
 
         Scanner input = new Scanner(System.in);
-        int [] [] A = new int [2] [3];
-        int [] [] B = new int [2][3];
+        int[][] A = new int[2][3];
+        int[][] B = new int[2][3];
 
         //Getting input from a matrix
-        for (int row = 0; row <2; row++) {
-            for (int col = 0; col < 3; col ++) {
-                A [row] [col] = input.nextInt();
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                A[row][col] = input.nextInt();
 
             }
         }
 
         // printing a matrix
         System.out.println("A = ");
-        for (int row = 0; row <2; row++) {
-            for (int col = 0; col < 3; col ++) {
-                A [row] [col] = input.nextInt();
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.println("  "+A[row] [col]);
+
+            }
+        }
+
+
 
 
     }
