3c1731a247af0097a80d7502f15db11317901e32
Mynul <mamun.harf@gmail.com>
2026-05-29 09:15:50 +0600

    comment adding

--- src/main/java/TwoDimentionArray.java
+++ src/main/java/TwoDimentionArray.java
@@ -7,10 +7,10 @@ public class TwoDimentionArray {
         int [] [] A = new int [2] [3];
         int [] [] B = new int [2][3];
 
+        //Getting input from a matrix
         for (int row = 0; row <2; row++) {
             for (int col = 0; col < 3; col ++) {
                 A [row] [col] = input.nextInt();
-
 
             }
         }
