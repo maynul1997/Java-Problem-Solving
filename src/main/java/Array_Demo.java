74b76de7153b54df57356caeb6decac5a9245e80
Mynul <mamun.harf@gmail.com>
2026-05-29 14:34:13 +0600

    for loop declere

--- src/main/java/Array_Demo.java
+++ src/main/java/Array_Demo.java
@@ -3,6 +3,15 @@ public class Array_Demo {
     static void main() {
         int [][] number = new int [4] [5];
 
+        for ( int row = 0; row < 4; row ++ ) {
+            for ( int col = 0; col < 5; col++ ) {
+
+
+            }
+
+
+        }
+
 
     }
 
