49381720ba73c0aa0e6cf8b6a4d3c6414664bcdc
Mynul <mamun.harf@gmail.com>
2026-05-30 00:34:09 +0600

    using for loop

--- src/main/java/Array7.java
+++ src/main/java/Array7.java
@@ -2,7 +2,13 @@ public class Array7 {
     static void main() {
         int [] [] number = new int [4] [];
         number[0] = new int [1];
+        number[1] = new int [2];
+        number[2] = new int [3];
+        number[3] = new int [4];
 
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0)
+        }
     }
 
 }
