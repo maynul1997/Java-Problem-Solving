7ff7de809efbc1031c359f9dadf22aec2d9b7c82
Mynul <mamun.harf@gmail.com>
2026-05-29 08:45:19 +0600

    makng matricx

--- src/main/java/ArrayType.java
+++ src/main/java/ArrayType.java
@@ -4,11 +4,13 @@ public class ArrayType {
         int [][] number = new int [2][4];
 
         number [0][0] = 10;
-        number [0][1] = 10;
-        number [0][2] = 10;
-        number [1][0] = 10;
-        number [1][1] = 10;
-        number [1][2] = 10;
+        number [0][1] = 20;
+        number [0][2] = 30;
+        number [1][0] = 40;
+        number [1][1] = 50;
+        number [1][2] = 60;
+
+
 
     }
 
