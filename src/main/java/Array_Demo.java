d516b7df32b62337fa257b3a1fd96aedc6a91302
Mynul <mamun.harf@gmail.com>
2026-05-29 14:37:38 +0600

    another variable declare

--- src/main/java/Array_Demo.java
+++ src/main/java/Array_Demo.java
@@ -2,16 +2,16 @@ public class Array_Demo {
 
     static void main() {
         int [][] number = new int [4] [5];
+        int k
 
         for ( int row = 0; row < 4; row ++ ) {
             for ( int col = 0; col < 5; col++ ) {
+                number [row][col] = k;
 
 
             }
 
-
         }
-
 
     }
 
