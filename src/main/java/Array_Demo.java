33bd311fa7d7f616e6c697fedc3d83e1670e8f59
Mynul <mamun.harf@gmail.com>
2026-05-29 14:39:15 +0600

    value declare

--- src/main/java/Array_Demo.java
+++ src/main/java/Array_Demo.java
@@ -2,7 +2,7 @@ public class Array_Demo {
 
     static void main() {
         int [][] number = new int [4] [5];
-        int k
+        int k = 0;
 
         for ( int row = 0; row < 4; row ++ ) {
             for ( int col = 0; col < 5; col++ ) {
