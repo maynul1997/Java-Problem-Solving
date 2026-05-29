c26e5e5d005825d5962b2fbd4d2eb23f1ee90abe
Mynul <mamun.harf@gmail.com>
2026-05-29 09:07:18 +0600

    taking scanner

--- src/main/java/TwoDimentionArray.java
+++ src/main/java/TwoDimentionArray.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class TwoDimentionArray {
     static void main() {
+
+        Scanner input = new Scanner(System.in);
         int [] [] A = new int [2] [3];
         int [] [] B = new int [2][3];
 
         for (int row = 0; row <2; row++) {
 
+            for (int col = 0; col < 3; col ++) {
+
+
+            }
         }
+
 
     }
 }
