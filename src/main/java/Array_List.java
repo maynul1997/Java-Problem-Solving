7381646c342ae09a074c11dbda28000566ffcfa2
Mynul <mamun.harf@gmail.com>
2026-07-28 00:05:13 +0600

    print new line

--- src/main/java/Array_List.java
+++ src/main/java/Array_List.java
@@ -10,9 +10,17 @@ public class Array_List {
 
         for (int i = 0; i<5; i++) {
 
-            System.out.print(" Ascending Number "+number[i]);
+            System.out.print("  "+number[i]);
 
         }
+
+        System.out.println();
+        for (int i = 4; i<5; i++) {
+
+            System.out.print("  "+number[i]);
+
+        }
+
 
     }
 }
