7e52cc4598998bc47c3284d1fb2b86ef44ad73be
Mynul <mamun.harf@gmail.com>
2026-07-27 23:04:39 +0600

    using Array sort method

--- src/main/java/Array_List.java
+++ src/main/java/Array_List.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class Array_List {
     static void main(String[] args) {
 
         int [] number = {5, -10, 7, 4, 90};
+
+        Arrays.sort(number);
 
     }
 }
