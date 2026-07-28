d41a0a6f50bbd74693843006dac8a4b7fa3bffda
Mynul <mamun.harf@gmail.com>
2026-07-28 23:45:50 +0600

    print aray size

--- src/main/java/ArrayList_1.java
+++ src/main/java/ArrayList_1.java
@@ -5,6 +5,7 @@ public class ArrayList_1 {
    public static void main() {
 
        ArrayList <Integer> number = new ArrayList<Integer>();
+       System.out.println("Size = "+number.size());
 
     }
 }
