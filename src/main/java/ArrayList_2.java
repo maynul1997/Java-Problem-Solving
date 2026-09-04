000ca8ab714fbadcf284b3b8db4f2f7a9d6dd669
Mynul <mamun.harf@gmail.com>
2026-09-05 00:11:51 +0600

    declare main method

--- src/main/java/ArrayList_2.java
+++ src/main/java/ArrayList_2.java
@@ -3,5 +3,13 @@ import java.util.ArrayList;
 public class ArrayList_2 {
     static void main() {
         ArrayList<Integer>  number = new ArrayList<>();
+
+        number.add(20);
+        number.add(-40);
+        number.add(50);
+        number.add(30);
+        number.add(90);
+        number.add(-20);
+
     }
 }
